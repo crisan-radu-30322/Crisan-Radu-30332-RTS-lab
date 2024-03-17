1280c436179651c1e6bcdf04b64242eec3dfee22
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-03-17 22:40:12 +0200

    Lab2

---
+++ LAB2/LAB2/src/ThreadInfo.java
@@ -0,0 +1,20 @@
+public class ThreadInfo {
+    int id;
+    int c;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
