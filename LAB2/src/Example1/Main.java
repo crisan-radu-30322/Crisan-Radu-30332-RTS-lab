eb1eddb9d0b1545f9eb0c4f583b18143828142f7
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-03-18 13:46:46 +0200

    Labsession2

---
+++ LAB2/src/Example1/Main.java
@@ -0,0 +1,14 @@
+package Example1;
+public class Main{
+    private static boolean stopThreads = false;
+    public static void main(String[] args){
+        FileService service = new FileService("message.txt");
+        RThread reader = new RThread(service);
+        WThread writer = new WThread(service);
+        reader.start();
+        writer.start();
+    }
+    public static boolean isStopThreads(){
+        return stopThreads;
+    }
+}
---
+++ LAB2/src/Example2/Consumer.java
@@ -0,0 +1,11 @@
+package Example2;
+
+public class Consumer extends Thread{
+    private Buffer bf;
+    Consumer(Buffer bf){this.bf = bf;}
+    public void run(){
+        while(true){
+            System.out.println("Consumer " + this.getName() + " recived >> " + bf.get());
+        }
+    }
+}
