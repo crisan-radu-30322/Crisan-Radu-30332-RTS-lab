a56a9ad642e06cf8a4c4c38a0bdf1cb4cee5a466
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-05-24 00:58:04 +0300

    LAB4

---
+++ LAB4/src/App1/ExecutionThread.java
@@ -0,0 +1,90 @@
+package App1;
+
+public class ExecutionThread extends Thread {
+    Integer monitor1, monitor2;
+    int sleepvalue, activity_max, activity_min;
+
+    public ExecutionThread(Integer monitor1, Integer monitor2, int sleepvalue, int activity_min, int activity_max) {
+        this.monitor1 = monitor1;
+        this.monitor2 = monitor2;
+        this.sleepvalue = sleepvalue;
+        this.activity_min = activity_min;
+        this.activity_max = activity_max;
+    }
+
+    public void run() {
+        System.out.println(this.getName() + " - STATE 1");
+        if (monitor1 != null) {
+            synchronized (monitor1) {
+                if (monitor2 != null) {
+                    synchronized (monitor2) {
+                        System.out.println(this.getName() + " - STATE 2");
+                        int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+                        for (int i = 0; i < k * 100000; i++) {
+                            i++;
+                            i--;
+                        }
+                    }
+                    try {
+                        Thread.sleep(Math.round((Math.random() * sleepvalue)) * 500);
+
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    System.out.println(this.getName() + " - STATE 3");
+                } else {
+                    System.out.println(this.getName() + " - STATE 2");
+                    int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+                    for (int i = 0; i < k * 100000; i++) {
+                        i++;
+                        i--;
+                    }
+                    try {
+                        Thread.sleep(Math.round((Math.random() * sleepvalue)) * 500);
+
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    System.out.println(this.getName() + " - STATE 3");
+                }
+            }
+        } else {
+            if(monitor2 != null){
+                synchronized (monitor2) {
+                    if (monitor1 != null) {
+                        synchronized (monitor1) {
+                            System.out.println(this.getName() + " - STATE 2");
+                            int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+                            for (int i = 0; i < k * 100000; i++) {
+                                i++;
+                                i--;
+                            }
+
+                        }
+                        try {
+                            Thread.sleep(Math.round((Math.random() * sleepvalue)) * 500);
+
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        System.out.println(this.getName() + " - STATE 3");
+                    } else {
+                        System.out.println(this.getName() + " - STATE 2");
+                        int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+                        for (int i = 0; i < k * 100000; i++) {
+                            i++;
+                            i--;
+                        }
+                        try {
+                            Thread.sleep(Math.round((Math.random() * sleepvalue)) * 500);
+
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        System.out.println(this.getName() + " - STATE 3");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LAB4/src/App1/Main.java
@@ -0,0 +1,12 @@
+package App1;
+
+public class Main {
+    public static void main(String[] args){
+        Integer monitor1 = new Integer(1);
+        Integer monitor2 = new Integer(2);
+        new ExecutionThread(monitor1,null,4,2,4).start();//0
+        new ExecutionThread(monitor1,monitor2,5,2,5).start();//1
+        new ExecutionThread(null,monitor2,3,3,6).start();//2
+
+    }
+}
