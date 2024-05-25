c3139a313d108f5b0924d8e74696ce7edfbc5a48
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-05-25 19:23:22 +0300

    lab4

---
+++ LAB4/src/App3/ExecutionThread.java
@@ -0,0 +1,34 @@
+package App3;
+
+public class ExecutionThread extends Thread{
+    Integer monitor;
+    int sleepvalue, activity_max, activity_min;
+
+    public ExecutionThread(Integer monitor, int sleepvalue, int activity_min, int activity_max){
+        this.monitor=monitor;
+        this.sleepvalue=sleepvalue;
+        this.activity_min=activity_min;
+        this.activity_max=activity_max;
+    }
+
+    public void run(){
+        while(true){
+            System.out.println(this.getName() + " - STATE 1");
+            synchronized (monitor){
+                int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+                for (int i = 0; i < k * 100000; i++) {
+                    i++;
+                    i--;
+                }
+            }
+            System.out.println(this.getName() + " - STATE 3");
+            try {
+                Thread.sleep(Math.round(Math.random() * sleepvalue * 500));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(this.getName() + " - STATE 4");
+
+        }
+    }
+}
