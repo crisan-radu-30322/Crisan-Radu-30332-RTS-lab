19f9491bc8e4c8c7f90f7454c7ae3b1e1f2f9e2e
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-06-02 18:58:12 +0300

    commit

---
+++ Lab_session5/src/L7App1A/Main.java
@@ -0,0 +1,73 @@
+package L7App1A;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ExecThread extends Thread{
+    Lock lock1, lock2;
+    int activityMin1, activityMin2, activityMax1, activityMax2;
+    int sleepVal;
+
+    CyclicBarrier barrier;
+    public ExecThread(CyclicBarrier barrier,Lock lock1, Lock lock2, int activityMin1, int activityMin2, int activityMax1, int activityMax2, int sleepVal){
+        this.barrier=barrier;
+        this.lock1=lock1;
+        this.lock2=lock2;
+        this.activityMin1=activityMin1;
+        this.activityMin2=activityMin2;
+        this.activityMax1=activityMax1;
+        this.activityMax2=activityMax2;
+        this.sleepVal=sleepVal;
+    }
+
+    public void run(){
+        System.out.println(this.getName() + " State 1");
+        int k = (int)Math.round(Math.random() * (activityMax1 - activityMin1) + activityMin1);
+        int i;
+        for (i = 0; i < k * 100000; i++) {
+            i++;
+            i--;
+        }
+        lock1.lock();
+        System.out.println(this.getName() + " State 2");
+        lock1.unlock();
+        lock2.lock();
+        System.out.println(this.getName() + " State 3");
+        k = (int) Math.round(Math.random() * (activityMax2 - activityMin2) + activityMin2);
+        for (i = 0; i < k * 100000; i++) {
+            i++;
+            i--;
+        }
+        lock2.unlock();
+        try {
+            Thread.sleep(500 * sleepVal);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(this.getName() + " State 4");
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(this.getName() + " has ended");
+    }
+}
+public class Main {
+    public static void main(String args[]) throws BrokenBarrierException, InterruptedException {
+        Lock lock1 =new ReentrantLock();
+        Lock lock2 = new ReentrantLock();
+        CyclicBarrier barrier = new CyclicBarrier(3);
+        while(true) {
+            new ExecThread(barrier, lock1, lock2, 2, 4, 4, 6, 4).start();
+            new ExecThread(barrier, lock1, lock2, 3, 5, 5, 7, 5).start();
+            barrier.await();
+            barrier.reset();
+            Thread.sleep(5000);
+        }
+    }
+}
---
+++ Lab_session5/src/L7App2B/Main.java
@@ -0,0 +1,79 @@
+package L7App2B;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+class ExecThread extends Thread{
+    Semaphore semaphore;
+
+    int activityMin, activityMax, sleepVal;
+
+    CyclicBarrier barrier;
+
+    public ExecThread(CyclicBarrier barrier,Semaphore semaphore, int activityMin, int activityMax, int sleepVal){
+        this.barrier =barrier;
+        this.semaphore=semaphore;
+        this.activityMin=activityMin;
+        this.activityMax=activityMax;
+        this.sleepVal=sleepVal;
+    }
+
+    public void run(){
+
+        System.out.println(this.getName() + " State 1");
+        if(this.getName() == "Thread-1") {
+            try {
+                semaphore.acquire(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            try {
+                this.semaphore.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(this.getName() + " State 2");
+        int k= (int) Math.round(Math.random() * (activityMax-activityMin) + activityMin);
+        for(int i=0; i<k*100000; i++){
+            i++;i--;
+        }
+        try {
+            Thread.sleep(500*sleepVal);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.semaphore.release();
+        System.out.println(this.getName() + " State 3");
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
+
+
+public class Main {
+    public static void main(String args[]) throws BrokenBarrierException, InterruptedException {
+        CyclicBarrier barrier = new CyclicBarrier(4);
+        Semaphore semaphore = new Semaphore(2);
+        while(true) {
+            new ExecThread(barrier, semaphore, 2, 4, 4).start();
+            new ExecThread(barrier, semaphore, 3, 6, 3).start();
+            new ExecThread(barrier, semaphore, 2, 5, 5).start();
+            barrier.await();
+            barrier.reset();
+        }
+    }
+}
+
+
